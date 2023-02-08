7f14b9a6ea16e00119b789b4bbfc4b3e0de2848d
ricardo-semiao <ricardo.semiao@outlook.com>
2023-02-08 20:27:40 -0300

    adicionei o texto "#alteração"

--- server.R
+++ server.R
@@ -3,6 +3,8 @@
 # Plotly event link 1: https://plot.ly/r/shinyapp-plotly-events/
 # Plotly event link 2: https://plot.ly/r/shiny-coupled-events/
 
+#alteração
+
 shinyServer(function(input, output, session) {
   
   ###############################################.
